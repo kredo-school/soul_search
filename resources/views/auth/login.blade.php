2364763091db690ae2e48ac2aa0f0e36fcd55e3c
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-02-27 20:46:47 +0900

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <h1 class="text-center mt-2">Login</h1>
+
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">

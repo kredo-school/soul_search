f4980591cffaa601cb880282008148f823269f22
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-02-27 21:03:45 +0900

    add fontawesome code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card"><i class="fa-regular fa-circle-user"></i>
                 <h1 class="text-center mt-2">Login</h1>
 
 

6eb48e458917a6ffa23d65733f1431ab1d377842
Shunsuke Tanaka <tk.sn9m@gmail.com>
2023-03-04 19:04:59 +0900

    Update HomeController to show only the tags that the user wants

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Chat;
 use App\Models\User;
+use App\Models\Tag;
 
 class HomeController extends Controller
 {
@@ -16,6 +17,7 @@ class HomeController extends Controller
      */
     private $chat;
     private $user;
+    private $tag;
 
     public function __construct(Chat $chat, User $user)
     {
@@ -31,10 +33,51 @@ class HomeController extends Controller
     public function index(){
         $all_chats = $this->chat->latest()->get();
 
-        //need to update to show only tagged chats a user wants
+        // Need to update to show only tagged chats a user wants
+        // Need to show main tags(max:3) and added tags
+        $tagged_chats = [];
+        $main_tags = $this->getMainTags();
+        $added_tags = $this->getAddedTags();
 
-        return view('home');
+        foreach($all_chats as $chat){
+            if($chat->tag->isMain() || $chat->tag->isAdded()){
+                $tagged_chats[] = $chat;
+            }
+        }
+
+        return view('home')
+            ->with('tagged_chats', $tagged_chats)
+            ->with('main_tags', $main_tags)
+            ->with('added_tags', $added_tags);
     }
+
+    private function getMainTags(){
+        $all_tags = $this->tag->all();
+        $main_tags = [];
+
+        foreach($all_tags as $tag){
+            if($tag->isMain()){
+                $main_tags[] = $tag;
+            }
+        }
+
+        return array_slice($main_tags, 0, 3);
+    }
+
+    private function getAddedTags(){
+        $all_tags = $this->tag->all();
+        $added_tags = [];
+
+        foreach($all_tags as $tag){
+            if($tag->isAdded()){
+                $added_tags[] = $tag;
+            }
+
+            return array($added_tags);
+        }
+    }
+
+
 
     public function showUser($id){
         $user = $this->user->findOrFail($id);

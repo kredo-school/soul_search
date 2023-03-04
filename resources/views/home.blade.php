3991a9cb58464a0157b6d60e9a70fa4ae56e5365
Shunsuke Tanaka <tk.sn9m@gmail.com>
2023-03-04 19:08:08 +0900

    Update the code to show the tags that the user wants insread of all  tags

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <!-- Navbar -->
-        <div class="h-100">
-            @include('layouts.side')
-        </div>
+    <!-- Navbar -->
+    <div class="col-2 h-100 bg-white">
+        @include('layouts.side')
+    </div>
 
-        <!-- Tags' bar -->
-        <!-- Chats -->
+    <!-- Tags' bar -->
+    <div class="col-2 h-100 bg-white">
+        <div class="mt-3">
+            <div class="row">
+                <p class="text-dark fw-bold tag-bar">Main</p>
+            </div>
+            @foreach ($main_tags as $tag)
+            <div class="row align-items-center m-3">
+                <div class="col-auto">
+                    <a href="#">
+                        <i class="fa-regular fa-hashtag"></i>
+                    </a>
+                </div>
+                <div class="col ps-0">
+                    <a href="#" class="text-decoration-none text-dark tag-bar">{{ $tag->tag }}</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="mt-3">
+            <div class="row">
+                <p class="text-dark fw-bold tag-bar">Fav</p>
+            </div>
+            @foreach ($added_tags as $tag)
+                <div class="row align-items-center m-3">
+                    <div class="col-auto">
+                        <a href="#">
+                            <i class="fa-regular fa-hashtag"></i>
+                        </a>
+                    </div>
+                    <div class="col ps-0">
+                        <a href="#" class="text-decoration-none text-dark tag-bar">{{ $tag->tag }}</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <!-- Chats -->
+    <div class="col-auto">
         <div class="row">
             <!-- Header -->
             <div class="w-100">
-                <i class="fa-regular fa-hashtag text-primary"></i>
-                <a href="{{ route('tag.store', $tag->id) }}" class="text-decoration-none text-dark">{{ $tag->tag }}</a>
+                <i class="fa-regular fa-hashtag"></i>
+                <a href="#" class="text-decoration-none text-dark tag-header">{{ $tag->tag }}</a>
             </div>
             <!-- Body (Need to update to show chats the user wants) -->
             <div class="ms-3">
@@ -34,7 +70,7 @@
                     <textarea name="chat" id="chat" rows="3" class="form-control" placeholder="Type your message #{{ $tag->tag }}">
                         {{ old('chat') }}
                     </textarea>
-                    <button type="submit" class="btn">Send</button>
+                    <button type="submit" class="btn btn-orange">Send</button>
                 </div>
                 @error('chat')
                     <div class="text-danger small">{{ $message }}</div>

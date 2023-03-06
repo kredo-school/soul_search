ba2e9bff58bbfb782e2cc30b9b8d2648be85f03e
Takahiro Nago <takahiro.nago.art@gmail.com>
2023-03-06 20:56:39 +0900

    user_tag table added

--- database/migrations/2023_03_06_115244_create_user_tag_table.php
+++ database/migrations/2023_03_06_115244_create_user_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CrateTagsTable extends Migration
+class CreateUserTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CrateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('user_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('tag');
             $table->unsignedBigInteger('user_id');
-            $table->timestamps();
-            $table->softDeletes();
+            $table->unsignedBigInteger('tag_id');
+            $table->boolean('is_main')
+                     ->default(0);
+            $table->timestamp('last_access')->useCurrent();
 
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('tag_id')->references('id')->on('tags');
         });
     }
 
@@ -31,6 +33,6 @@ class CrateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('user_tag');
     }
 }

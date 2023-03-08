3ea76c162350ce99eec7e222f78f534e929137db
jezielramos <jezielramos02@gmail.com>
2023-03-08 20:08:40 +0800

    login and register frontend modifications

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <img src="{{ asset('img/logo.png')}}" class="float-left mt-2">
-</div>
+</div> -->
+<div class="container"><h1 class="text-center fw-bold mb-3">  <img src="{{ asset('img/logo_modified.png')}}" class="float-left mt-2" width="450" height="120"></h1></div>
+
     <div class="container mb-3">
         <div class="row justify-content-center">
             <div class="col-md-5">
-                <div class="card"><i class="fa-regular fa-circle-user text-center mt-3 icon-lg text-orange"></i>
-                    <h1 class="text-center mt-3">{{ __('Login') }}</h1>
-
-
+                <div class="card border-0">
+                <div class = "justify-content-center text-center">
+                    <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="4.8"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" fill="white"></rect> <path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9999 6C9.79077 6 7.99991 7.79086 7.99991 10C7.99991 12.2091 9.79077 14 11.9999 14C14.209 14 15.9999 12.2091 15.9999 10C15.9999 7.79086 14.209 6 11.9999 6ZM17.1115 15.9974C17.8693 16.4854 17.8323 17.5491 17.1422 18.1288C15.7517 19.2966 13.9581 20 12.0001 20C10.0551 20 8.27215 19.3059 6.88556 18.1518C6.18931 17.5723 6.15242 16.5032 6.91351 16.012C7.15044 15.8591 7.40846 15.7251 7.68849 15.6097C8.81516 15.1452 10.2542 15 12 15C13.7546 15 15.2018 15.1359 16.3314 15.5954C16.6136 15.7102 16.8734 15.8441 17.1115 15.9974Z" fill="#ababab"></path> </g></svg>                    
+                        <!-- <i class="fa-regular fa-circle-user text-center mt-3 icon-lg text-orange"></i> -->
+                </div>
+                    <h2 class="text-center fw-bold mt-3">{{ __('Login') }}</h2>
+                            {{--  Message  --}}
+                            <p class="text-center">Do not have an account?
+                                <a href="{{ route('register') }}" class="text-center text-orange text-decoration-none">Sign up here!</a>
+                            </p>
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
@@ -18,12 +26,11 @@
                             {{--  Email  --}}
                             <div class="row mb-3">
                                 <div>
-                                    <label for="email" class="col-md-4 col-form-label ">{{ __('Email Address') }}</label>
+                                    <label for="email" class="col-md-4 col-form-label">{{ __('Email Address') }}</label>
                                 </div>
 
                                 <div class="col-md-12">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -52,34 +59,35 @@
                             {{--  Login button  --}}
                             <div class="row mb-0">
                                 <div class="d-grid gap-2 mt-3">
-                                    <button type="submit" class="btn btn-orange text-white">
+                                    <button type="submit" class="btn btn-orange text-white btn-block mb-2">
                                         {{ __('Login') }}
                                     </button>
                                 </div>
 
-                                <div class="row justify-content-center">
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            <p class="text-center mt-3">{{ __('Forgot Your Password?') }}</p>
-                                        </a>
-                                    @endif
-                                </div>
                             </div>
 
                             {{--  Horizontal line  --}}
                             <div class="text-divider text-muted">or</div>
 
                             {{--  Facebook Logo  --}}
-                            <div class="container justify-content-center logo-center">
+                            <!-- <div class="container justify-content-center logo-center">
                                 <a class="" href="#">
-                                    <img class="icon-sm justify-content-center mt-3" src="{{asset('img/btn_facebook.svg')}}" alt="">
+                                    <img class="icon-sm justify-content-center mt-3 mb-4" src="{{asset('img/btn_facebook.svg')}}" alt="">
                                 </a>
+                                
+                            </div> -->
+                            <div class="d-grid gap-2 mt-3">
+                                <a class="btn btn-primary btn-block mb-2 border-0" style="background-color: #1877F2;" href="#!" role="button"><i class="fab fa-facebook me-2"></i>Facebook</a>
                             </div>
-                            {{--  Message  --}}
-                            <p class="mt-3 mb-4 text-center">Do not have an account?
-                                <a href="{{ route('register') }}" class="text-center text-orange text-decoration-none">Sing up here!</a>
-                            </p>
 
+                            
+                            <div class="row justify-content-center">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link text-decoration-none" href="{{ route('password.request') }}">
+                                            <p class="text-center mt-3">{{ __('Forgot Your Password?') }}</p>
+                                        </a>
+                                    @endif
+                            </div>
                         </form>
                     </div>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,39 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <img src="{{ asset('img/logo.png')}}" class="float-left mt-2">
-</div>
+</div> -->
 
-<div class="container"><h1 class="text-center fw-bold mb-3">Crate Your Account</h1></div>
+<!-- <div class="container"><h1 class="text-center fw-bold mb-3">Create Your Account</h1></div> -->
+<div class="container"><h1 class="text-center fw-bold mb-3">  <img src="{{ asset('img/logo_modified.png')}}" class="float-left mt-2" width="450" height="120"></h1></div>
 
 {{--  Card body  --}}
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card mb-4">
+        <div class="col-md-5">
+            <div class="card mb-4 border-0">
                 <div class="card-body">
-
-                    <div class="row justify-content-center">
-                        <div class="col-8 mb-3 btn btn-orange">
-                            <span class="text-white">Profile&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                            <span class="text-white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tags</span>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                            <button class="btn btn-orange form-control text-white btn-block">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-1-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002H7.971L6.072 5.385v1.271l1.834-1.318h.065V12h1.312V4.002Z"/>
+                                    </svg>
+                                    Profile
+                                </button>
+                            </div>
+                            <div class="col">
+                                <button class="btn btn-orange form-control text-white btn-block">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-2-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24c0-.691.493-1.306 1.336-1.306.756 0 1.313.492 1.313 1.236 0 .697-.469 1.23-.902 1.705l-2.971 3.293V12h5.344v-1.107H7.268v-.077l1.974-2.22.096-.107c.688-.763 1.287-1.428 1.287-2.43 0-1.266-1.031-2.215-2.613-2.215-1.758 0-2.637 1.19-2.637 2.402v.065h1.271v-.07Z"/>
+                                    </svg>
+                                    Tags
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    </div>                            
+                            <!-- <span class="text-white">Profile&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span class="text-white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tags</span> -->
 
-                    <h2 class="text-center fw-bold">Your Profile</h2>
-                    <p class="text-muted text-center">Enter the login information for your accoun.<br>You can edit it after registering.</p>
+                            <!-- <span class="text-white">Profile&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                            <span class="text-white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tags</span> -->
+
+                    <h2 class="text-center fw-bold mt-3">{{ __('Create Your Account') }}</h2>
+                    <p class="text-muted text-center">Enter the login information for your account.<br>You can edit it after registering.</p>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
-                        {{--  Username  --}}
-                        <div class="row mb-3 justify-content-center">
-                            <div>
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
-                        </div>
+                        
+                        <!-- {{--  Username  --}}
+                        <div class="row mb-3">
+                        <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                             <div class="col-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -41,9 +58,25 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> -->
 
-                        {{--  Email  --}}
+                        {{--  Username  --}}
+                            <div class="row mb-3 ">
+                                <div>
+                                    <label for="name" class="col-md-2 col-form-label">{{ __('Username') }}</label>
+                                </div>
+
+                                <div class="col-md-12">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        <!-- {{--  Email  --}}
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
@@ -56,8 +89,24 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> -->
+                        {{--  Email  --}}
+                            <div class="row mb-3">
+                                <div>
+                                    <label for="email" class="col-md-2 col-form-label">{{ __('Email') }}</label>
+                                </div>
 
+                                <div class="col-md-12">
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+<!-- 
                         {{--  Password  --}}
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
@@ -71,8 +120,26 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div> -->
 
+                        {{--  Password  --}}
+                            <div class="row mb-3">
+                                <div>
+                                    <label for="password" class="col-md-2 col-form-label">{{ __('Password') }}</label>
+                                </div>
+
+                                <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+<!-- 
                         {{--  Password confirmation  --}}
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Password Confirmation') }}</label>
@@ -80,25 +147,52 @@
                             <div class="col-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
-                        </div>
+                        </div> -->
 
-                        {{--  Next button  --}}
+                        {{--  Password Confirm --}}
+                            <div class="row mb-2">
+                                <div>
+                                    <label for="password" class="col-md-6 col-form-label">{{ __('Password Confirmation') }}</label>
+                                </div>
+
+                                <div class="col-md-12">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+
+                         
+                                </div>
+                            </div>
+
+
+                        <!-- {{--  Next button  --}}
                         <div class="row mb-0 justify-content-center">
                             <div class="d-grid gap-2 mt-3 col-6 ">
                                 <button type="submit" class="btn btn-orange text-white mb-3">
                                     {{ __('Next') }}
                                 </button>
                             </div>
+                        </div> -->
+
+                        {{--  Next button  --}}
+                            <div class="row mb-0">
+                                <div class="d-grid gap-2 mt-3">
+                                    <button type="submit" class="btn btn-orange text-white btn-block mb-2">
+                                        {{ __('Next') }}
+                                    </button>
+                                </div>
+
                         </div>
 
                         {{--  Horizontal line  --}}
                         <div class="text-divider text-muted">or</div>
 
                         {{--  Facebook Logo  --}}
-                        <div class="container justify-content-center logo-center">
+                        <!-- <div class="container justify-content-center logo-center">
                             <a class="" href="#">
                                 <img class="icon-sm justify-content-center mt-3" src="{{asset('img/btn_facebook.svg')}}" alt="">
                             </a>
+                        </div> -->
+                        <div class="d-grid gap-2 mt-3">
+                                <a class="btn btn-primary btn-block mb-2 border-0" style="background-color: #1877F2;" href="#!" role="button"><i class="fab fa-facebook me-2"></i>Facebook</a>
                         </div>
                         {{--  Message  --}}
                         <p class="mt-3 mb-4 text-center">Already have an account?

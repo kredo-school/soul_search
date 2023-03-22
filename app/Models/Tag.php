a9c7692205e7f55fe2f68d9508795f04d1fd4657
Shunsuke Tanaka <tk.sn9m@gmail.com>
2023-03-22 21:07:40 +0900

    no message

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Testing\Constraints\SoftDeletes;
-use Illuminate\Support\Facades\Auth;
 
 class Tag extends Model
 {

59d6ea7403270cb695dd60a06dae158d7065d89b
Shunsuke Tanaka <tk.sn9m@gmail.com>
2023-03-31 21:58:12 +0900

    no message

--- app/helpers.php
+++ app/helpers.php
@@ -22,19 +22,6 @@ function getMainTags(){
     return $user->userTag()->where('tag_category', config('enums')['tag_category']['main'])->with('tag')->take(3)->get();
 }
 
-// function getMainTags(){
-
-//     $all_tags = Tag::all();
-//     $main_tags = [];
-
-//     foreach($all_tags as $main_tag){
-//         if($main_tag->isMain()){
-//             $main_tags[] = $main_tag;
-//         }
-//     }
-//     return array_slice($main_tags, 0, 3);
-// }
-
 function getFavTags(){
     $user = Auth::user();
     $all_tags = Tag::all();
@@ -47,17 +34,3 @@ function getFavTags(){
     }
     return $user->userTag()->where('tag_category', config('enums')['tag_category']['favorite'])->with('tag')->get();
 }
-
-// function getFavTags(){
-
-//     $all_tags = Tag::all();
-//     $fav_tags = [];
-
-//     foreach($all_tags as $fav_tag){
-//         if($fav_tag->isFav()){
-//             $fav_tags[] = $fav_tag;
-//         }
-//     }
-//     return $fav_tags;
-// }
-

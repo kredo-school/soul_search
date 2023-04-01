ca3fb268d12b6bd498d86ab676145aab6230b793
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-04-01 18:55:35 +0900

    I fixed UI

--- resources/views/admin/admin_side.blade.php
+++ resources/views/admin/admin_side.blade.php
@@ -4,7 +4,7 @@
 
 <ul class="nav nav-pills flex-column ms-0 mb-auto mt-5">
     <li class="my-1">
-        <a href="#" class="nav-link active" aria-current="page">
+        <a href="{{ route('admin.users') }}" class="nav-link active" aria-current="page">
             <i class="fa-solid fa-user"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users
         </a>
     </li>
@@ -20,35 +20,19 @@
         <i class="fa-solid fa-unlock hide-700"></i>
     </button>
     <ul class="dropdown-menu">
-
-        @if (Route::has('login'))
-            <li class="ps-3">
-                <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-        @endif
-
-        @if (Route::has('register'))
-            <li class="dropdown-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-        @endif
-
-        @else
         <li class="dropdown-item dropdown">
             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }}
             </a>
+            <a class="" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
         </li>
-
-        <a class="" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
     </ul>
 </div>

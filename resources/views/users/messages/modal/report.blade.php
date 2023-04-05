a0c8669022e83463400d74c2a2796e0d2f25b2b4
Takahiro Nago <takahiro.nago.art@gmail.com>
2023-04-06 00:56:18 +0900

    change nbsp to margin

--- resources/views/users/messages/modal/report.blade.php
+++ resources/views/users/messages/modal/report.blade.php
@@ -15,19 +15,19 @@
                         <div class="mb-2">
                             <input type="checkbox" class="form-check-input" id="comms" name="violation_types[]" value="communication">
                             <label for="comms" class="form-check-label fw-bold">Comms Abuse</label>
-                            <div class="text-muted text-sm">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Offensive Language, Hateful Speech, Sexual Harassment</div>
+                            <div class="text-muted text-sm ms-3">Offensive Language, Hateful Speech, Sexual Harassment</div>
                         </div>
 
                         <div class="mb-2">
                             <input type="checkbox" class="form-check-input" id="names" name="violation_types[]" value="name">
                             <label for="names" class="form-check-label fw-bold">Offensive or Inappropriate Names</label>
-                            <div class="text-muted text-sm">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Username, Tag</div>
+                            <div class="text-muted text-sm ms-3">Username, Tag</div>
                         </div>
 
                         <div class="mb-3">
                             <input type="checkbox" class="form-check-input" id="threats" name="violation_types[]" value="threat">
                             <label for="threats" class="form-check-label fw-bold">Threats</label>
-                            <div class="text-muted text-sm">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Off-App Physical and Emotional Abuse, Doxxing, Bullying</div>
+                            <div class="text-muted text-sm ms-3">Off-App Physical and Emotional Abuse, Doxxing, Bullying</div>
                         </div>
 
                         <div class="text-center">

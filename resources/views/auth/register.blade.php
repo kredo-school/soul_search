3fd1bc1c0f4bf827ee597d68d78895db4b0152db
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-04-05 22:31:28 +0900

    fix script

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,8 +6,9 @@
 
 @section('title','Create Account')
 
+@section('scripts')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
+@endsection
 
 @section('content')
 

8218753058fc8ae6d93fd407ad517822ec6c8aba
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-04-13 19:36:36 +0900

    fix admin side bar

--- resources/views/admin/admin_side.blade.php
+++ resources/views/admin/admin_side.blade.php
@@ -2,15 +2,17 @@
     <img src="{{ asset('images/logo.svg')}}" class="m-3 hide-700">
 </a>
 
-<ul class="nav nav-pills flex-column ms-0 mb-auto mt-5">
-    <li class="my-1">
-        <a href="{{ route('admin.users') }}" class="nav-link active" aria-current="page">
+<ul class="nav nav-pills flex-column ms-0 mb-auto mt-4">
+    <li class="nav-item py-2 sidebar-selected fw-bold">
+        <a href="{{ route('admin.users') }}" class="flex-fill nav-link link-dark" aria-current="page">
             <i class="fa-solid fa-user"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users
         </a>
     </li>
     <li class="my-1">
         <a href="#" class="nav-link link-dark">
-            <i class="fa-regular fa-file"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Posts
+            <i class="fa-regular fa-file"></i>
+                <span class="text-dark hide-800">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Posts
         </a>
     </li>
 </ul>

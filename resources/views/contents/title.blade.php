5f28ffdf81500ff21548fa3aa2590d659df6d106
ShT9m <tk.sn9m@gmail.com>
2023-04-19 22:30:29 +0900

    no message

--- resources/views/contents/title.blade.php
+++ resources/views/contents/title.blade.php
@@ -2,9 +2,9 @@
     <!-- User Avatar -->
     <a href="{{ route('profiles.index') }}">
         @if ($chat->user->avatar)
-            <img src="{{ asset('/storage/avatars/' . $chat->user->avatar) }}" alt="{{ $chat->user->avatar }}" class="rounded-circle icon-sm user-avatar">
+            <img src="{{ asset('/storage/avatars/' . $chat->user->avatar) }}" alt="{{ $chat->user->avatar }}" class="rounded-circle user-avatar">
         @else
-            <i class="fa-solid fa-circle-user fa-4x text-secondary icon-sm"></i>
+            <i class="fa-solid fa-circle-user fa-4x text-secondary"></i>
         @endif
     </a>
 </div>

2b3b0940e39820d8be1df9615f5a5b47c8af41d3
ShT9m <tk.sn9m@gmail.com>
2023-04-22 16:07:52 +0900

    Revamp User's home to show an array of their close friends

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,7 @@ class HomeController extends Controller
         $recent_tags = getRecentTags();
         $main_tags = getMainTags();
         $fav_tags = getFavTags();
+        $close_users = $this->showCloseUsers();
 
         // Need to fix to reflect the update of migrations
         foreach($tagged_chats as $chat){
@@ -38,24 +39,32 @@ class HomeController extends Controller
 
         return view('home')
             ->with('tag', $tag)
+            ->with('close_users', $close_users)
             ->with('tagged_chats', $tagged_chats)
             ->with('recent_tags', $recent_tags)
             ->with('main_tags', $main_tags)
             ->with('fav_tags', $fav_tags);
     }
 
-    public function showCloseUsers(User $user)
+    public function showCloseUsers()
     {
         $all_users = User::latest()->get();
+        $close_users = [];
 
-        $authUser = Auth::user();
+        $user = Auth::user();
 
-        $pivot_items = collect($authUser->messagesSent)->merge($authUser->messagesReceived)->sortBy('pivot.created_at')
+        $pivot_items = collect($user->messagesSent)->merge($user->messagesReceived)->sortBy('pivot.created_at')
         ->filter(function($a) use($user){
             return $a->pivot->sender_id == $user->id || $a->pivot->receiver_id == $user->id;
         });
 
-        return view('home', compact('user', 'all_users', 'pivot_items'));
-    }
+        foreach($all_users as $user){
+            if($user->id !== Auth::user()->id){
+                $close_users[] = $user;
+            }
+        }
 
+        // return view('home', compact('user', 'all_users', 'pivot_items'));
+        return array_slice($close_users, 0, 8);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,58 +9,98 @@
 @section('content')
     <div class="d-flex justify-content-start p-0">
         @if(Auth::user()->userTag()->exists())
-            @auth
-            <!-- Tags' bar -->
-            <div class="col-2 ps-1 bg-white tag-bar border">
-                <div class="mt-5">
-                    <p class="text-dark fw-bold mb-1 ms-3 tag-name">Recent</p>
-                    <ul class="nav nav-pills flex-column px-0">
-                        @foreach ($recent_tags as $recent_tag)
-                            <li class="nav-item mb-1">
-                                <a href="{{ route('chats.show', $recent_tag->tag->id) }}" class="flex-fill nav-link">
-                                    <i class="fa-regular fa-hashtag"></i>
-                                    <span class="text-dark tag-name">{{ $recent_tag->tag->name }}</span>
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <div class="mt-5">
-                    <p class="text-dark fw-bold mb-1 ms-3 tag-name">Main</p>
-                    <ul class="nav nav-pills flex-column px-0">
-                        @foreach ($main_tags as $main_tag)
-                            <li class="nav-item mb-1">
-                                <a href="{{ route('chats.show', $main_tag->tag->id) }}" class="flex-fill nav-link">
-                                    <i class="fa-regular fa-hashtag"></i>
-                                    <span class="text-dark tag-name">{{ $main_tag->tag->name }}</span>
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <div class="mt-5">
-                    <p class="text-dark fw-bold mb-1 ms-3 tag-name">Fav</p>
-                    <ul class="nav nav-pills flex-column px-0">
-                        @foreach ($fav_tags as $fav_tag)
-                            <li class="nav-item mb-1">
-                                <a href="{{ route('chats.show', $fav_tag->tag->id) }}" class="flex-fill nav-link">
-                                    <i class="fa-regular fa-hashtag"></i>
-                                    <span class="text-dark tag-name">{{ $fav_tag->tag->name }}</span>
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+        @auth
+        <!-- Tags' bar -->
+        <div class="col-2 ps-1 bg-white tag-bar border">
+            <div class="mt-5">
+                <p class="text-dark fw-bold mb-1 ms-3 tag-name">Recent</p>
+                <ul class="nav nav-pills flex-column px-0">
+                    @foreach ($recent_tags as $recent_tag)
+                        <li class="nav-item mb-1">
+                            <a href="{{ route('chats.show', $recent_tag->tag->id) }}" class="flex-fill nav-link">
+                                <i class="fa-regular fa-hashtag"></i>
+                                <span class="text-dark tag-name">{{ $recent_tag->tag->name }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
-            @endauth
-            <!-- Home for a specific user -->
-            <div class="col text-center">
-                <p class="text-muted h5" style="transform: translateY(40vh)">Hello {{ Auth::user()->username }}!</p>
+            <div class="mt-5">
+                <p class="text-dark fw-bold mb-1 ms-3 tag-name">Main</p>
+                <ul class="nav nav-pills flex-column px-0">
+                    @foreach ($main_tags as $main_tag)
+                        <li class="nav-item mb-1">
+                            <a href="{{ route('chats.show', $main_tag->tag->id) }}" class="flex-fill nav-link">
+                                <i class="fa-regular fa-hashtag"></i>
+                                <span class="text-dark tag-name">{{ $main_tag->tag->name }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="mt-5">
+                <p class="text-dark fw-bold mb-1 ms-3 tag-name">Fav</p>
+                <ul class="nav nav-pills flex-column px-0">
+                    @foreach ($fav_tags as $fav_tag)
+                        <li class="nav-item mb-1">
+                            <a href="{{ route('chats.show', $fav_tag->tag->id) }}" class="flex-fill nav-link">
+                                <i class="fa-regular fa-hashtag"></i>
+                                <span class="text-dark tag-name">{{ $fav_tag->tag->name }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        @endauth
+        <!-- Home for a specific user -->
+        <div class="col px-5 pt-5">
+            <div class="card">
+                <div class="card-header bg-white border">
+                    <div class="row align-items-center">
+                        <p class="text-muted h4 pt-3">Hello {{ Auth::user()->username }}!</p>
+                    </div>
+                </div>
+                <!-- Show Close Users-->
+                @if ($close_users)
+                @foreach($close_users as $user)
+                    <div class="card-body border border-1">
+                        <div class="row justify-content-center">
+                            <div class="col-auto" style="width: 5.5rem">
+                                <a href="{{ route('messages.show', $user->id) }}">
+                                    @if ($user->avatar)
+                                        <img src="{{ asset('/storage/avatars/'. $user->avatar) }}" class="rounded-circle user-avatar" alt="{{ $user->avatar }}">
+                                    @else
+                                        <i class="fa-solid fa-circle-user fa-4x text-secondary"></i>
+                                    @endif
+                                </a>
+                            </div>
+                            <div class="col ps-0 pt-1">
+                                <div class="row justify-content-center">
+                                    <div>
+                                        <a href="{{ route('messages.show', $user->id) }}" class="h5 text-decoration-none fw-bold tag-name">
+                                            {{ $user->username }}
+                                        </a>
+                                    </div>
+                                    <div>
+                                        @if ($user->is_active)
+                                            <p class="chat-text"><i class="fa-solid fa-globe text-info"></i>&nbsp;Online</p>
+                                        @else
+                                            <p class="chat-text"><i class="fa-solid fa-circle-xmark text-danger"></i>&nbsp;Offline</p>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                @endif
             </div>
         @else
             <div class="col text-center">
                 <p class="text-muted h5" style="transform: translateY(39vh); line-height: 1.5em;">You don't have any tags yet.<br>When you register tags, they'll on your home.</p>
             </div>
         @endif
+        </div>
     </div>
 @endsection

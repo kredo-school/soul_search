8a0a4eb3744c021dcd555d27c700352c4cc32a33
1609KAZUYA <yellow19870407@yahoo.co.jp>
2023-04-25 21:00:27 +0900

    fixed icon color

--- resources/views/admin/admin_side.blade.php
+++ resources/views/admin/admin_side.blade.php
@@ -5,7 +5,7 @@
 <ul class="nav nav-pills flex-column ms-0 mb-auto mt-4">
     <li class="nav-item py-2 sidebar-selected fw-bold">
         <a href="{{ route('admin.users') }}" class="flex-fill nav-link link-dark" aria-current="page">
-            <i class="fa-solid fa-user"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users
+            <i class="fa-solid fa-user text-orange"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Users
         </a>
     </li>
     <li class="my-1">
